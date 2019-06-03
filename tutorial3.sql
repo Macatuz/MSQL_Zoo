4e41480e44973009d22013c2c948c2d7cde5b0ec
Armando Jose Soto Melo <armando29@live.com>
2019-06-03 14:34:24 -0400

    Complete SQLZoo Tutorial 3

--- tutorial3.sql
+++ tutorial3.sql
@@ -38,7 +38,30 @@ SELECT winner
 SELECT winner FROM nobel
  WHERE winner LIKE 'John%'
  
- --------------------------------------------------------
+--------------------------------------------------------
  
- select yr, subject, winner FROM nobel
+select yr, subject, winner FROM nobel
 where (subject = 'Chemistry'  and yr = 1984 or subject = 'Physics' and yr = 1980)  
+
+--------------------------------------------------------
+ 
+select yr, subject, winner from nobel 
+where yr = 1980 and subject != 'Chemistry' and subject != 'Medicine'
+
+
+---------------------------------------------------------
+
+select yr, subject, winner from nobel
+where (subject = 'Medicine' and yr < 1910) or (subject = 'Literature' and yr >= 2004)
+
+---------------------------------------------------------
+
+Nobel Quiz
+
+1. Option 5
+1. Option 3
+1. Option 2
+1. Option 3
+1. Option 3
+1. Option 3
+1. Option 4
